16f5611f94916292889e960d7d84620c5261027f
SeungHeon <otekgi@naver.com>
2025-05-22 02:14:11 +0900

    layout5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import Navbar from '../components/Navbar'
 import './globals.css'
 
+
+export const metadata = {
+  title: "오사카공립대학 한국인유학생회",  
+  description: "오사카공립대학 한국인유학생회 홈페이지입니다",};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
@@ -11,3 +16,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
+
